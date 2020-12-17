25a7181295dbc2cf65004d5afe7d71e0719b8822
nicksandoval <sand8val@gmail.com>
2020-12-16 22:34:41 -0800

    fixed some wording for descriptions

--- src/containers/Projects/ProjectsInfo/ProjectsInfo.js
+++ src/containers/Projects/ProjectsInfo/ProjectsInfo.js
@@ -8,20 +8,20 @@ import cookieImg from '../../../assets/whatcookie.png'
 const iSpace = {
     url: ispaceImg,
     title: 'Social Network Application (Handlebars/MySQL)',
-    description: 'This application allows a user to stay connected to friends, family, & more. User can create an account, search for other users to add as friends. User can view and post on their friends wall as well. The application utilizes Node/Express back-end with MySQL database and uses Handlebars, JQuery, HTML5, CSS Bootstrap Framework and on the front-end.',
+    description: "This application allows a user to create an account, make a profile, and search & add friends/family. User can post on their own wall and their friend's walls. This application utilizes Node/Express back-end with MySQL database, Passport for session, & Bcrypt for password hashing. Handlebars, JQuery, HTML5, & CSS Bootstrap Framework is used on the front-end.",
     github: 'https://github.com/nsando84/iSpace'
 };
 
 const iHostel = {
     url: ihostelImg,
     title: 'Hotel Booking Application (React/MongoDB)',
-    description: 'Application connects a Traveler to an Innkeeper for easy booking. Users can host as Innkeepers, or book as Travelers. Hostel selection is by location, capacity, date-range, etc..Application uses React Hooks/Context API with React-Bootstrap on front-end. Backend API is provided by NodeJS/Express. Secure login is provided by Bcrypt for password hashing and JsonWebToken to handle sessions. Data-layer persistence is provided by NoSQL DB - MongoDB Atlas via ODB, Mongoose.',
+    description: 'Application connects a Traveler to an Innkeeper for easy booking. Users can host as Innkeepers, or book as Travelers. Hostel selection is by location, capacity, date-range, etc..Application uses React Hooks/Context API with React-Bootstrap on front-end. Backend API is provided by NodeJS/Express. Secure login is provided by Bcrypt for password hashing and JsonWebToken to handle sessions. Data-layer persistence is provided by NoSQL DB - MongoDB Atlas via ODM, Mongoose.',
     github: 'https://github.com/nsando84/iHostel'
 };
 
 const usaf = {
     url: airforceImg,
-    title: 'Unofficial Airforce PT Tracker (jQuery/MongoDB)',
+    title: 'Unofficial Airforce PT Tracker (Handlebars/MongoDB)',
     description: "Application allows user to insert values and app assigns points to track fitness level against set standards(USAF Fitness Test Scoring). Uses ChartJS for data visualization and IndexedDB to cache local recent searches. Additional, technologies include Handlebars for content and MongoDB for key/value store.",
     github: 'https://github.com/nsando84/Fitness-Score-Tracker-DB'
 };
@@ -29,7 +29,7 @@ const usaf = {
 const employeeDirectory = {
     url: employeeImg,
     title: 'Employee Management Directory (React/MongoDB)',
-    description: 'Employee Management Application. App uses CRUD design capabilities. Client-side uses React class-components, service-layer API uses NodeJS/Express, and data-layer persistence is provided by NoSQL DB - MongoDB Atlas via ODB, Mongoose.',
+    description: 'Employee Management Application. App uses CRUD design capabilities. Client-side uses React class-components, service-layer API uses NodeJS/Express, and data-layer persistence is provided by NoSQL DB - MongoDB Atlas via ODM, Mongoose.',
     github: 'https://github.com/nsando84/Employee-Directory-Now'
 };
 
