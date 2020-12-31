a403091571050fad4298e3df42a1f85b60589d68
nicksandoval <sand8val@gmail.com>
2020-12-30 22:50:35 -0800

    testing a

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import Navbar from './components/navbar/Navbar'
+import React, { useEffect } from 'react';
+import Navbar from './components/navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 import Home from './containers/Home';
 import Contact from './containers/Contact';
-import Projects from './containers/Projects/Projects'
+import Projects from './containers/Projects/Projects';
 
 
 
+ReactGA.initialize('G-453W5RDG98')
+const browserHistory = createBrowserHistory()
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search)
+})
 
 function App() {
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <BrowserRouter>
 
